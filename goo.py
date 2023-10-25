dc9641b79213afab274a6fa1c618a97c7d448024
ByLele <2516066049@qq.com>
2023-10-25 22:19:53 +0800

    20231025

--- goo.py
+++ goo.py
@@ -1,3 +1,4 @@
+# *utf-8*
 from googleapiclient.discovery import build
 from google_auth_oauthlib.flow import InstalledAppFlow
 from google.auth.transport.requests import Request
@@ -7,20 +8,32 @@ import re
 import os
 import pickle
 import pprint
-
+import json
+import yaml
+import sys
 SCOPES = ["https://www.googleapis.com/auth/youtube.force-ssl"]
+import logging
+logging.basicConfig(
+    level=logging.DEBUG,
+    format='%(asctime)s - %(levelname)s - [%(filename)s] - %(message)s',
+    handlers=[
+        logging.StreamHandler(),  # Console handler
+    ]
+)
 
+# Create a logger object
+logger = logging.getLogger(__name__)
 def youtube_authenticate():
     os.environ["OAUTHLIB_INSECURE_TRANSPORT"] = "1"
     api_service_name = "youtube"
     api_version = "v3"
-    client_secrets_file = "./client_secret_523219218819-cjaggat5t7bkju2m4ss1e6lge2c8calu.apps.googleusercontent.com.json"
-    #client_secrets_file = "./client_hgfcbn_secret_847585282559-3j95bf9plmbcij3csa1dg2penimmntpc.apps.googleusercontent.com.json"
+    #client_secrets_file = "./client_secret_523219218819-cjaggat5t7bkju2m4ss1e6lge2c8calu.apps.googleusercontent.com.json"
+    client_secrets_file = "./client_hgfcbn_secret_847585282559-3j95bf9plmbcij3csa1dg2penimmntpc.apps.googleusercontent.com.json"
     creds = None
     # the file token.pickle stores the user's access and refresh tokens, and is
     # created automatically when the authorization flow completes for the first time
-    if os.path.exists("token.pickle"):
-        with open("token.pickle", "rb") as token:
+    if os.path.exists("token3.pickle"):
+        with open("token3.pickle", "rb") as token:
             creds = pickle.load(token)
     # if there are no (valid) credentials availablle, let the user log in.
     if not creds or not creds.valid:
@@ -30,7 +43,7 @@ def youtube_authenticate():
             flow = InstalledAppFlow.from_client_secrets_file(client_secrets_file, SCOPES)
             creds = flow.run_local_server(port=0)
         # save the credentials for the next run
-        with open("token.pickle", "wb") as token:
+        with open("token3.pickle", "wb") as token:
             pickle.dump(creds, token)
 
     return build(api_service_name, api_version, credentials=creds)
@@ -63,9 +76,36 @@ def get_sub(youtube,**kwargs):
         part="snippet",
         #mySubscribers=True,
         mine=True,
-        maxResults=50
+        maxResults=50,
+        **kwargs
     ).execute()
 
+
+def uper_yaml_write(uper_data:dict) -> None:
+    
+    if len(uper_data) ==  0:
+        logger.info()
+
+    for item in uper_data:
+        #pprint.pprint(item)
+        filename = item.get("snippet").get("title")
+        # json_data = json.dumps(item,enco,ensure_ascii=False)
+        uper_data = {}
+        uper_data["author"] = filename
+        uper_data["kind"] = item.get("kind")
+        uper_data["etag"] = item.get("etag")
+        uper_data["snippet"] = item.get("snippet")
+
+        
+        print(filename)
+        filename = filename +".yaml"
+        filename = "./uper/"+filename 
+        try:
+            with open(filename,"w",encoding='utf-8') as f:
+                yaml.dump(uper_data,f,allow_unicode=True)
+        except Exception as e:
+            print(e)
+        print("\n \n")
 # # authenticate to YouTube API
 # youtube = youtube_authenticate() 
 # video_url = "https://www.youtube.com/watch?v=jNQXAC9IVRw&ab_channel=jawed"
@@ -85,21 +125,23 @@ if __name__ == "__main__":
     # parse video ID from URL
     video_id = get_video_id_by_url(video_url)
     # make API call to get video info
-    #response = get_video_details(youtube, id=video_id)
+    response = get_video_details(youtube, id=video_id)
     
-    #pprint.pprint(response)
+    pprint.pprint(response)
     
     channellist = get_sub(youtube)
-    print(video_id)
+    items = channellist.get("items")
+    uper_yaml_write(items)
+    while channellist.get("nextPageToken"):
+        
+        channellist = get_sub(youtube,pageToken=channellist.get("nextPageToken"))
+        items = channellist.get("items")
+        uper_yaml_write(items)
+        
+
     # print(channellist)
-    print(channellist.items())
-    print("="*20)
-    if channellist.items():
-        for item in channellist.items():
-            pprint.pprint(item)
-    # for item in channellist.items():
-    #     pprint.pprint(item.to_dict())
-    #pprint.pprint(channellist.items().to_dict())
+    # print(channellist.items())
+    # print("="*20)
 
 
 
