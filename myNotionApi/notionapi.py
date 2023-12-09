6ce25eea74efb1857c90e0d61c8703ff8f7571cf
ByLele <2516066049@qq.com>
2023-12-10 00:03:27 +0800

    1209

--- myNotionApi/notionapi.py
+++ myNotionApi/notionapi.py
@@ -3,7 +3,9 @@ import json
 from datetime import datetime, timedelta
 import requests
 from pprint import pprint
-from uper_data_reader import uper_content_load
+
+
+
 from utils.notion_util import _json
 from myNotionApi.uper_data_reader import uper_data_obj
 
